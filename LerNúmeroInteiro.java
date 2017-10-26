254c0ecdda9d6227778fa3d9071a34a16d26d9e7
diogo2002 <32169278+diogo2002@users.noreply.github.com>
2017-10-26 17:29:53 +0100

    Add files via upload

---
+++ LerNúmeroInteiro.java
@@ -0,0 +1,14 @@
+package Pacote3;
+import java.io.*;
+public class LerNúmeroInteiro {
+    
+	public static void main(String[] args)  throws IOException {
+	{String C= "";
+System.out.print("Digite um digito:");
+InputStreamReader LerS = new InputStreamReader (System.in);
+BufferedReader GuardaS = new BufferedReader (LerS);
+C = GuardaS.readLine();
+System.out.printf("Digitou:%d", Integer.valueOf(C));}}
+}
+
+	
