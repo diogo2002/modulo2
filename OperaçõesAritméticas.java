d3acc9f18e47aa8ae2344384969e7e78ea44c5b6
diogo2002 <32169278+diogo2002@users.noreply.github.com>
2017-10-26 17:07:54 +0100

    Add files via upload

---
+++ OperaçõesAritméticas.java
@@ -0,0 +1,13 @@
+package Pacote2;
+
+public class OperaçõesAritméticas {
+
+	public static void main(String[] args) {
+	
+		System.out.printf("%d\n",4+3*(15-2));
+        System.out.printf("%f\n",40.0/9);
+        System.out.printf("%d\n",4/3);
+
+	}
+
+}
