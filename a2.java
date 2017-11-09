fbfd04c0b3a4d710fa2d33c506f42a240e913836
diogo2002 <32169278+diogo2002@users.noreply.github.com>
2017-11-09 16:38:43 +0000

    Add files via upload

---
+++ a2.java
@@ -0,0 +1,19 @@
+package a;
+import java.util.Scanner;
+public class a2 {
+
+	public static void main(String[] args) {
+		double c1, c2, h;
+System.out.print("C1?");
+Scanner LerS=new Scanner(System.in);
+c1=LerS.nextDouble();
+
+System.out.print("C2?");
+c2=LerS.nextDouble();
+
+h=Math.sqrt(c1*c1+c2*c2);
+
+System.out.printf("H=%.0f", h);
+	}
+
+}
