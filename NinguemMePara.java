60c698da923f0b1a0c83b56bd7eaaac791290e5a
diogo2002 <32169278+diogo2002@users.noreply.github.com>
2017-11-24 15:21:44 +0000

    Add files via upload

---
+++ NinguemMePara.java
@@ -0,0 +1,18 @@
+package OlaVoltei;
+
+public class NinguemMePara {
+
+	public static void main(String[] args) {
+    
+	 int soma=0;
+    for (int I=1 ; I<=10 ;I++) {
+    	soma+=I;
+    	System.out.println(I);
+    }
+    System.out.println(soma);
+    
+    
+
+	}
+
+}
