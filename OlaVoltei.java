12e9a5f0d39dd3979815c25430cde21b6e4e94b5
diogo2002 <32169278+diogo2002@users.noreply.github.com>
2017-11-24 15:00:23 +0000

    Add files via upload

---
+++ OlaVoltei.java
@@ -0,0 +1,11 @@
+package OlaVoltei;
+
+public class OlaVoltei {
+
+	public static void main(String[] args) {
+		 final String mensagem= "Hoje é sexta feira";
+		 for ( int I=1 ; I <=5 ; I++ )
+         System.out.println(mensagem);
+	}
+
+}
